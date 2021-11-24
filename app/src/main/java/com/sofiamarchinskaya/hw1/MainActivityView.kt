58eccf5b4f2dbc9a9e92bcc0e3cce0358d78c38c
Marchinskaya Sofia <sonamarcinskaa@gmail.com>
2021-11-24 23:11:07 +0300

    additions for task 2

--- app/src/main/java/com/sofiamarchinskaya/hw1/MainActivityView.kt
+++ app/src/main/java/com/sofiamarchinskaya/hw1/MainActivityView.kt
@@ -3,4 +3,7 @@ package com.sofiamarchinskaya.hw1
 interface MainActivityView {
     fun onSaveComplete()
     fun onFailed()
+    fun openAboutScreen()
+    fun shareNote(title : String, text : String)
+
 }
