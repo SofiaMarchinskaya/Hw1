2a085c600f0369c9de5f5996f58535ec32a2f130
sofia_marchinskya <plank03@mail.ru>
2022-01-13 20:52:18 +0300

    исправление

--- app/src/main/java/com/sofiamarchinskaya/hw1/presenters/framework/NotesPagerPresenter.kt
+++ app/src/main/java/com/sofiamarchinskaya/hw1/presenters/framework/NotesPagerPresenter.kt
@@ -5,6 +5,8 @@ import com.sofiamarchinskaya.hw1.models.entity.Note
 interface NotesPagerPresenter {
 
     fun init()
+
     fun onNoteSaved(id: Long)
+
     fun listCollected(list: List<Note>, id: Long): Int
 }
