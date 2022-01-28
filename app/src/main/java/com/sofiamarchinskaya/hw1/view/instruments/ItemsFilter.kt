f5c45a7bb1032d0e71343a79a752a321657b5853
sofia_marchinskya <plank03@mail.ru>
2022-01-28 14:05:28 +0300

    исправления

--- app/src/main/java/com/sofiamarchinskaya/hw1/view/instruments/ItemsFilter.kt
+++ app/src/main/java/com/sofiamarchinskaya/hw1/view/instruments/ItemsFilter.kt
@@ -3,9 +3,9 @@ package com.sofiamarchinskaya.hw1.view.instruments
 import com.sofiamarchinskaya.hw1.models.entity.Note
 
 class ItemsFilter : QueryFilter<Note> {
-    override fun filter(query: String?, list: List<Note>?): List<Note>? =
+    override fun filter(query: String, list: List<Note>?): List<Note>? =
         list?.filter {
-            it.title.contains(query.toString(), ignoreCase = true)
-                    || it.body.contains(query.toString().lowercase(), ignoreCase = true)
+            it.title.contains(query, ignoreCase = true)
+                    || it.body.contains(query, ignoreCase = true)
         }
 }
--- app/src/main/java/com/sofiamarchinskaya/hw1/view/instruments/QueryFilter.kt
+++ app/src/main/java/com/sofiamarchinskaya/hw1/view/instruments/QueryFilter.kt
@@ -2,5 +2,5 @@ package com.sofiamarchinskaya.hw1.view.instruments
 
 interface QueryFilter<T> {
 
-    fun filter(query: String?, list: List<T>?): List<T>?
+    fun filter(query: String, list: List<T>?): List<T>?
 }
